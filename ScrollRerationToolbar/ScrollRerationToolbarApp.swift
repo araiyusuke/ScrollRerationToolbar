054a2a823c8fe476832016dc8a7c5a712241b88a
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-23 18:39:58 +0900

    Initial Commit

---
+++ ScrollRerationToolbar/ScrollRerationToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollRerationToolbarApp.swift
+//  ScrollRerationToolbar
+//
+//  Created by 名前なし on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollRerationToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
